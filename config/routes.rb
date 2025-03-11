3fbec9c90c00b352d2c7744efd9552a5bc454507
darren277 <darren277@yahoo.com>
2025-03-11 20:33:30 +0000

    Simplified controller and routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
-Rails.application.routes.draw do
-  scope '', as: 'kitten_plugin' do
-    scope 'projects/:project_id', as: 'project' do
-      resources :kittens
-    end
-  end
-
-  # Create a route that is handled completely in the frontend
-  # with a helper controller that just renders a plain page for the frontend
-  # to hook into with its own route
-  get 'my_plugin', to: 'my_plugin#index'
+OpenProject::MyPlugin::Engine.routes.draw do
+  root to: 'my_plugin#index'
 end
+
