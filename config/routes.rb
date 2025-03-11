fe6d51a74a62769869d0acf7470a4b32d01a6f2c
darren277 <darren277@yahoo.com>
2025-03-11 22:30:35 +0000

    KPI dashboard and gamification routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   get '/my_plugin', to: 'my_plugin#index'
+
+  # KPI Dashboard routes
+  get '/kpi_dashboard', to: 'kpi_dashboard#index'
+  get '/kpi_dashboard/data', to: 'kpi_dashboard#data'
+  
+  # Gamification routes
+  get '/gamification', to: 'gamification#index'
+  get '/gamification/:user_id', to: 'gamification#show'
 end
