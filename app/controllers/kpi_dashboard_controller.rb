f01f9cc69b2097e95a26319e2e9064133d875a16
darren277 <darren277@yahoo.com>
2025-03-11 20:46:12 +0000

    KPI dashboard controller.

---
+++ app/controllers/kpi_dashboard_controller.rb
@@ -0,0 +1,18 @@
+class KpiDashboardController < ApplicationController
+  layout 'angular'
+  
+  def index
+    # This will render your Angular app
+  end
+  
+  def data
+    # Example endpoint to provide data to your Angular component
+    kpis = {
+      completed_tasks: WorkPackage.where(status: 'closed').count,
+      story_points_completed: WorkPackage.sum(:story_points),
+      # Add other metrics as needed
+    }
+    
+    render json: kpis
+  end
+end
