c341dad76d88659843ecf91f3eb3d15dc392cc85
darren277 <darren277@yahoo.com>
2025-03-11 19:24:59 +0000

    Updated routes.

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   # Create a route that is handled completely in the frontend
   # with a helper controller that just renders a plain page for the frontend
   # to hook into with its own route
-  get '/angular_kittens', to: 'angular#empty_layout'
+  get 'my_plugin', to: 'my_plugin#index'
 end
