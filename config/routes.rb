ebbed674e7482e8179c0b812341d7355e1718ac9
darren277 <darren277@yahoo.com>
2025-03-12 17:43:32 +0000

    Updated routes for project specific views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/my_plugin', to: 'my_plugin#index'
 
-  # Gamification routes
+  # Gamification routes - non-project context
   scope 'gamification' do
     get '/', to: 'gamification#index', as: 'gamification'
     get 'profile/:id', to: 'gamification#show', as: 'gamification_profile'
@@ -9,10 +9,20 @@ Rails.application.routes.draw do
     get 'leaderboard', to: 'gamification#leaderboard', as: 'leaderboard'
     get 'achievements', to: 'gamification#achievements', as: 'achievements'
   end
-  
-  # KPI dashboard routes
-  scope 'kpi_dashboard' do
-    get '/', to: 'kpi_dashboard#index', as: 'kpi_dashboard'
-    get 'data', to: 'kpi_dashboard#data', as: 'kpi_dashboard_data'
+
+  # Project-specific gamification routes
+  scope 'projects/:project_id', as: 'project' do
+    scope 'gamification' do
+      get '/', to: 'gamification#index', as: 'gamification'
+      # Add other project-specific gamification routes as needed
+    end
+  end
+
+  # KPI dashboard routes - project context only
+  scope 'projects/:project_id', as: 'project' do
+    scope 'kpi_dashboard' do
+      get '/', to: 'kpi_dashboard#index', as: 'kpi_dashboard'
+      get 'data', to: 'kpi_dashboard#data', as: 'kpi_dashboard_data'
+    end
   end
 end
